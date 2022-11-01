ba5cfc4e925716f921fcae59d21372f811333a08
SMPIT CODER <smpitalfityan2020@gmail.com>
2022-11-01 16:14:14 +0700

    relation

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,8 +3,9 @@
 
 @section('boddy')
 <article>
-    <h2>{{ $post->title }}</h2>
-    <p>{!! $post->body !!}</p>
+    <h2 class="mb-5">{{ $post->title }}</h2>
+    <p>By: Sandika Galih in {{  $post->categori->name  }}</p>
+    {!! $post->body !!}
 </article>
 <a href="/post">back to post</a>
 @endsection
